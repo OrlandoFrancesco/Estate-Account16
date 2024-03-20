416a2fd0cc4bed000e3bb6acdbc9b43e1937be58
OrlandoFrancesco <francesco.orlando99@libero.it>
2024-03-20 10:05:13 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': "ESTATE_ACCOUNT",
+    'version': '16.0',
+    'author': "Francesco",
+    'description': """Modulo estate_account""",
+    'depends': ['estate', 'account']
+}
---
+++ models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, Command
+
+class EstateProperty(models.Model):
+    _inherit="estate.property"
+
+    def button_sold(self):
+        self.env['account.move'].create({
+            'partner_id':self.partner_id.id,
+            'move_type':'out_invoice',
+            'name':self.name,
+            'invoice_line_ids':[
+                Command.create({
+                    'quantity': 1.0,
+                    'price_unit' : self.best_price
+                }),
+
+                Command.create({
+                    'name':'Commission',
+                    'quantity' : 1.0,
+                    'price_unit': ( self.best_price * 6 ) / 100
+                }),
+
+                Command.create({
+                    'name':'Administrative fees',
+                    'quantity': 1.0,
+                    'price_unit': 100.0
+                })
+            ]
+        })
+        
+        return super().button_sold()
