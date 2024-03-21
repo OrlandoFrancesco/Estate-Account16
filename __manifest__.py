a06045ef7a9c1b4e89fead17160d4dd8e3733e6d
OrlandoFrancesco <francesco.orlando99@libero.it>
2024-03-21 14:57:34 +0100

    PDF report

--- __manifest__.py
+++ __manifest__.py
@@ -3,5 +3,8 @@
     'version': '16.0',
     'author': "Francesco",
     'description': """Modulo estate_account""",
-    'depends': ['estate', 'account']
+    'depends': ['estate', 'account'],
+    'data':[
+        'report/estate_property_reports.xml'
+    ]
 }
