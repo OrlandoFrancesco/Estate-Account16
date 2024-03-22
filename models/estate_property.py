ff3ed3f70e2cbea3f050612f02e5f44b9fc2af72
OrlandoFrancesco <francesco.orlando99@libero.it>
2024-03-22 16:12:15 +0100

    Imports updated

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,27 +1,27 @@
-from odoo import models, fields, Command
+from odoo import models, Command
 
 class EstateProperty(models.Model):
     _inherit="estate.property"
 
     def button_sold(self):
         self.env['account.move'].create({
-            'partner_id':self.partner_id.id,
-            'move_type':'out_invoice',
-            'name':self.name,
-            'invoice_line_ids':[
+            'partner_id': self.partner_id.id,
+            'move_type': 'out_invoice',
+            'name': self.name,
+            'invoice_line_ids': [
                 Command.create({
                     'quantity': 1.0,
-                    'price_unit' : self.best_price
+                    'price_unit': self.best_price
                 }),
 
                 Command.create({
-                    'name':'Commission',
-                    'quantity' : 1.0,
+                    'name': 'Commission',
+                    'quantity': 1.0,
                     'price_unit': ( self.best_price * 6 ) / 100
                 }),
 
                 Command.create({
-                    'name':'Administrative fees',
+                    'name': 'Administrative fees',
                     'quantity': 1.0,
                     'price_unit': 100.0
                 })
