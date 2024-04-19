2387eb2963369eec1c5b8e115fc584ac27a92150
Francesco Orlando <133336016+OrlandoFrancesco@users.noreply.github.com>
2024-04-19 15:22:42 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
-{
+e{
     'name': "ESTATE_ACCOUNT",
     'version': '16.0',
     'author': "Francesco",
     'description': """Modulo estate_account""",
-    'depends': ['Estate16', 'account'],
+    'depends': ['estate', 'account'],
     'data':[
         'report/estate_property_reports.xml'
     ]
